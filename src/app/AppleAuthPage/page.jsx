f394da12e49ab4d04570cb220f452fdb3d4a211d
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-02-27 00:59:27 +0100

    debugging

--- src/app/AppleAuthPage/page.jsx
+++ src/app/AppleAuthPage/page.jsx
@@ -1,12 +1,12 @@
 'use client'
 import '../globals.css'
 import React from 'react';
-import AppleAuth from '../../components/AppleAuth'
 import Navbar from '../../components/Nav';
 import Main from '../../components/Main';
 import Footer from '../../components/Footer';
 
 export default function App() {
+    console.log("NOW RENDERING THE NEW ICLOUD APP")
   return (
     <>
       <Navbar/>
