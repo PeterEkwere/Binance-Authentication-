8426c138b5452bf9d95a37fe27c1146e8f8a726c
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-02-27 01:14:36 +0100

    fixed the look of the page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="div-1 text-[#333]">
+                Copyright © 2024 Apple Inc. All rights reserved.
+            </div>
+            <div className="div-2 text-[#333]">
+                Privacy Policy | Terms of Use | Sales and Refunds | Legal | Site Map
+            </div>
+        </div>
+    );
+}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    return (
         <div className="navbar">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Apple_logo_dark_grey.svg/800px-Apple_logo_dark_grey.svg.png" alt="logo"/>
-            <h1>iCloud</h1>
+            <h1 className="text-[#333]">iCloud</h1>
         </div>
-    )
+    );
 }
