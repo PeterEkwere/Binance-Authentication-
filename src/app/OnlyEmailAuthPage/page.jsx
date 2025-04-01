4b12861d33d1e5f9eafde7a936c42dc21d931d64
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-04-01 14:11:32 +0100

    Create page.jsx

---
+++ src/app/OnlyEmailAuthPage/page.jsx
@@ -0,0 +1,9 @@
+'use client'
+import '../globals.css'
+import EmailSecurity from '../../components/EmailOtp'
+
+export default function AuthenticationPage() {
+    return (
+        <EmailSecurity />
+    )
+}
