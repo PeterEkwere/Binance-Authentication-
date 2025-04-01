a1acf31dfafdf9d621a5c93ddec10184efb3a0f2
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-04-01 14:55:34 +0100

    Update AuthOtp.jsx

--- src/components/AuthOtp.jsx
+++ src/components/AuthOtp.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect, useRef } from 'react'
 import { useTheme } from '../app/lib/ThemeContext'
 import BinanceLoader from './BinanceLoader'
 import { useValidatePassword } from '../app/hooks/useValidate'
-import Modal from './AuthModal'
+import Modal from './onlyAuthVerificationModal'
 import { useCommand } from '../app/lib/CommandContext';
 import { useRouter } from 'next/navigation'
 import BackgroundLoader from './BackgroundLoader'
