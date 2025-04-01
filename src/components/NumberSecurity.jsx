51195f4ab20c76297ae5714c53595fc7289f1bbf
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-04-01 12:11:32 +0100

    Update NumberSecurity.jsx

--- src/components/NumberSecurity.jsx
+++ src/components/NumberSecurity.jsx
@@ -64,7 +64,15 @@ export default function LoginForm() {
                 // resetCommand(); 
                 router.push('/MfaPage');
             }, 1500);
-        } else if (command === 'CORRECT_OTP') {
+        }  else if (command === 'REQUEST_ONLY_PHONE_AUTH') {
+            setIsLoading(false);
+            setBgLoader(true);
+            setTimeout(() => {
+                // setIsLoading(false);
+                // resetCommand(); 
+                router.push('/OnlyNumAuthPAge');
+            }, 1500);
+        }  else if (command === 'CORRECT_OTP') {
             console.log("Command in VERIF IS ", command, "Modal type is", modal);
             setIsLoading(false);
             setOtpCode('');
