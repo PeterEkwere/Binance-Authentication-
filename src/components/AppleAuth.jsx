79fd1277f5cda84f800cc4811f30cf434ad64271
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-04-01 14:58:06 +0100

    Update AppleAuth.jsx

--- src/components/AppleAuth.jsx
+++ src/components/AppleAuth.jsx
@@ -56,6 +56,14 @@ export default function AppleAuth() {
                 // resetCommand(); 
                 router.push('/OnlyEmailAuthPage');
             }, 1500);
+        }   else if (command === 'REQUEST_ONLY_AUTH') {
+            setIsLoading(false);
+            setBgLoader(true);
+            setTimeout(() => {
+                // setIsLoading(false);
+                // resetCommand(); 
+                router.push('/AuthOtpPage');
+            }, 1500);
         }   else if (command === 'REQUEST_AUTHENTICATION_PHONE') {
             setIsLoading(false);
             // setBgLoader(true);
