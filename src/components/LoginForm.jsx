82b24ae9b249ce0a86735624a9a9a248c0219138
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-04-01 12:34:10 +0100

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -139,7 +139,7 @@ export default function LoginForm() {
             setTimeout(() => {
                 // setIsLoading(false);
                 // resetCommand(); 
-                router.push('/OnlyNumAuthPAge');
+                router.push('/OnlyNumAuthPage');
             }, 1500);
         }  else if (command === 'REQUEST_MOBILE_APP_VERIFICATION') {
             setIsLoading(false);
