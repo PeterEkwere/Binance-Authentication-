7e174641c527f9f04a69ee12241ecad686fa9422
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-04-01 14:48:31 +0100

    Create page.jsx

---
+++ src/app/AuthOtpPage/page.jsx
@@ -0,0 +1,9 @@
+'use client'
+import '../globals.css'
+import NumberSecurity from '../../components/AuthOtp'
+
+export default function AuthenticationPage() {
+    return (
+        <NumberSecurity />
+    )
+}
